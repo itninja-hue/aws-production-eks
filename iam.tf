91fbb5ab56e50533774683fbe9b6f7d512571c9f
aladin <aladin.harb@box2home.fr>
2020-02-20 10:02:37 +0100

    iam roles

---
+++ iam.tf
@@ -0,0 +1,62 @@
+# IAM role for Eks master nodes
+resource "aws_iam_role" "eks-master" {
+  name = "eks-master-role"
+  assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "eks.amazonaws.com"
+            },
+                "Action": "sts:AssumeRole"
+        }
+    ] 
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "Cluster-Policy" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+    role       = "${aws_iam_role.eks-master.name}"
+}
+
+ resource "aws_iam_role_policy_attachment" "Service-Policy" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+    role       = "${aws_iam_role.eks-master.name}"
+}
+
+# IAM role for Eks worker nodes
+resource "aws_iam_role" "eks-workers" {
+  name = "eks-worker-roles"
+  assume_role_policy = <<POLICY
+{
+   "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ] 
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "workers-Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = "${aws_iam_role.eks-workers.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "cni-Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = "${aws_iam_role.eks-workers.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr-Policy" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+    role       = "${aws_iam_role.eks-workers.name}"
+}
