a0a3b3f6fd1e8faa813a738b8a032baa50804f9e
aladin <aladin.harb@box2home.fr>
2020-03-26 20:24:27 +0100

    remove excessive security group and rule

--- sg-rules.tf
+++ sg-rules.tf
@@ -8,13 +8,3 @@ resource "aws_security_group_rule" "ingress-master-bastion" {
   to_port = 443
   type ="ingress"
 }
-
-resource "aws_security_group_rule" "ingress-worker-bastion" {
-  description = "Allow worker nodes to recive ssh connection from Bastion"
-  from_port = 22
-  protocol = "tcp"
-  source_security_group_id = "${aws_security_group.eks-bastion.id}"
-  security_group_id = "${aws_security_group.eks-worker.id}"
-  to_port = 22
-  type = "ingress"
-}
